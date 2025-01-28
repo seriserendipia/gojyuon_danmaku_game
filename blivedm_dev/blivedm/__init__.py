7e085b59fde2aec1326572a07e01b23f0a4b3481
seri <seriseri@163.com>
2025-01-27 22:36:11 -0800

    blivedm引用的就不放了

--- blivedm_dev/blivedm/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from .bdm_client import *
-from .bdm_handlers import *
-from .bdm_models import *
--- blivedm_dev/blivedm/bdm_handler_interface.py
+++
@@ -1,7 +0,0 @@
-class HandlerInterface:
-    """
-    直播消息处理器接口
-    """
-
-    async def handle(self, client, command: dict):
-        raise NotImplementedError
